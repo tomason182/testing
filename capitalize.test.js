dd815b115c0c5fefce880f6399f230b1f911e25d
tomason182 <elotro182@hotmail.com>
2024-05-24 19:45:43 -0300

    add some test as example

---
+++ capitalize.test.js
@@ -0,0 +1,17 @@
+const capitalize = require('./capitalize');
+
+test ('should exits', () => {
+    expect(capitalize()).toBeDefined();
+});
+
+test ('testing should return Testing', () => {
+    expect(capitalize('testing')).toBe('Testing');
+});
+
+test ('TESTING should return Testing', () => {
+    expect(capitalize('TESTING')).toBe('Testing');
+})
+
+test ('tesTing should return Testing', () => {
+    expect(capitalize('tesTing')).toBe('Testing');
+})
