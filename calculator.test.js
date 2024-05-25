443bb55ef4eac7c3e4f70f8d7b60005f2eda9e6f
tomason182 <elotro182@hotmail.com>
2024-05-25 11:03:49 -0300

    add: Error test

--- calculator.test.js
+++ calculator.test.js
@@ -15,3 +15,27 @@ test ('multiply', () => {
 test ('divide', () => {
     expect (calculator. divide(8, 2)).toBe(4);
 })
+
+// Handle errors.
+
+test('sum num and string', () => {
+    expect(() => calculator.add(2, 'abc')).toThrow('Both a or b are not numbers');
+});
+
+test('subtract num and string', () => {
+    expect(() => calculator.subtract(2, 'abc')).toThrow('Both a or b are not numbers');
+});
+
+test('multiply num and string', () => {
+    expect(() => calculator.multiply(2, 'abc')).toThrow('Both a or b are not numbers');
+});
+
+test('divide num and string', () => {
+    expect(() => calculator.divide(2, 'abc')).toThrow('Both a or b are not numbers');
+});
+
+// Division by Zero error handle
+
+test('zero division', () => {
+    expect(() => calculator.divide(4, 0)).toThrow('Division by Zero error');
+});
