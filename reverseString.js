a69bfbbfa0c9ce25767a01578a5f457492ae9be0
tomason182 <elotro182@hotmail.com>
2024-05-25 10:14:07 -0300

    fix: if conditional string

--- reverseString.js
+++ reverseString.js
@@ -1,11 +1,9 @@
 function reverseString(string) {
-    if(typeof string !== string) {
-        throw new error ('The input need to be a string type')
+    if(typeof string !== 'string') {
+        throw new Error ('The input needs to be of string type')
     }
     return string.split('').reverse().join('');
 }
 
+
 module.exports = reverseString;
-
-
-console.log(reverseString(234));
