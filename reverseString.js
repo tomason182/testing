9494dea07b99e5e78faf6467bffb247672d4a7b3
tomason182 <elotro182@hotmail.com>
2024-05-24 23:58:39 -0300

    add: not a string error handle

--- reverseString.js
+++ reverseString.js
@@ -1,5 +1,11 @@
 function reverseString(string) {
+    if(typeof string !== string) {
+        throw new error ('The input need to be a string type')
+    }
     return string.split('').reverse().join('');
 }
 
 module.exports = reverseString;
+
+
+console.log(reverseString(234));
