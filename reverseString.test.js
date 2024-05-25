6f3119cdad9d1aeab6c6f0ba5f10e281ad31c292
tomason182 <elotro182@hotmail.com>
2024-05-25 10:14:35 -0300

    add: Error handleing test

--- reverseString.test.js
+++ reverseString.test.js
@@ -4,3 +4,11 @@ const reverseString = require('./reverseString');
 test ('testing should return gnitset', () => {
     expect(reverseString('testing')).toBe('gnitset');
 });
+
+test ('testing sentences' , () => {
+    expect (reverseString('this is a sencence')).toBe('ecnecnes a si siht');
+})
+
+test ('testing non string type', () =>{
+    expect(() => reverseString(123)).toThrow('The input needs to be of string type');
+});
